2b2efe3272104d436e499d1d58b3bc74a3b15723
Shuyang Sun <shuyangsun10@gmail.com>
2023-09-29 20:55:41 -0400

    YuNetDeploy forward prop working.

--- mmdet/models/detectors/yunet_deploy.py
+++ mmdet/models/detectors/yunet_deploy.py
@@ -1,11 +1,11 @@
 import torch
 
 from ..builder import DETECTORS
-from .single_stage import SingleStageDetector
+from .single_stage_deploy import SingleStageDetectorDeploy
 
 
 @DETECTORS.register_module()
-class YuNetDeploy(SingleStageDetector):
+class YuNetDeploy(SingleStageDetectorDeploy):
     def __init__(
         self, backbone, neck, bbox_head, train_cfg=None, test_cfg=None, pretrained=None
     ):
